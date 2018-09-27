a77a177a61d1a4c1c5fca7d2357eb85fbe8ac9c9
Mark Buenviaje <mbuenviaje@outlook.com>
2018-09-26 22:37:46 -0700

    not working yet

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name, devoured) VALUES ('Mark', true);
+INSERT INTO burgers (name, devoured) VALUES ('Ryan Manson', true);
+INSERT INTO burgers (name, devoured) VALUES ('Camille', true);
